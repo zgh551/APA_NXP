ad532058d2fbdacdab88474567340e7d7ce49f9b
朱国华 <zgh_email@163.com>
2018-12-07 11:01:25 +0800

    add system work class and configure the system work module

---
+++ Module/inc/SystemWork.h
@@ -0,0 +1,63 @@
+/*
+ * SystemWork.h
+ *
+ *  Created on: 2018Äê12ÔÂ5ÈÕ
+ *      Author: zhuguohua
+ */
+
+#ifndef SYSTEMWORK_H_
+#define SYSTEMWORK_H_
+
+#include "Property.h"
+#include "derivative.h"
+#include "project.h"
+#include "Vehicle.h"
+
+typedef enum _WorkingState
+{
+	Debug = 0,
+	Calibration,
+	Test,
+	Normal
+}WorkingState;
+
+typedef enum _DubugState
+{
+	DirectControl = 0,
+	SpeedControl,
+	ChangAnControl,
+	UltrasonicSR
+}DubugState;
+
+class SystemWork : public Vehicle
+{
+
+public:
+	SystemWork();
+	SystemWork(float dt,float kp,float ki,float kd,float i_lim,float out_lim);
+	SystemWork(float dt,float kp,float ki,float kd,float i_lim,float out_lim,float threshold);
+	virtual ~SystemWork();
+
+	void SystemWorkState(void);
+	void DubugStataMahine(void);
+//	/*** Variabel Property ***/
+//	/* System State */
+	uint8_t getWorkingModule();
+	void setWorkingModule(uint8_t value);
+	Property<SystemWork,uint8_t,READ_WRITE> WorkingModule;
+
+	uint8_t getFunctionStatus();
+	void setFunctionStatus(uint8_t value);
+	Property<SystemWork,uint8_t,READ_WRITE> FunctionStatus;
+
+private:
+	/*** System state ***/
+	uint8_t _working_module;
+	uint8_t _function_status;
+
+	/*** System Working State Machine ***/
+	WorkingState _system_working_state;
+};
+
+
+#endif /* SYSTEMWORK_H_ */
---
+++ Module/src/SystemWork.cpp
@@ -0,0 +1,136 @@
+/*
+ * SystemWork.cpp
+ *
+ *  Created on: 2018Äê12ÔÂ5ÈÕ
+ *      Author: zhuguohua
+ */
+
+#include "SystemWork.h"
+
+
+SystemWork::SystemWork() {
+	// TODO Auto-generated constructor stub
+	_system_working_state = Debug;
+
+	//// System Status //////
+	WorkingModule.setContainer(this);
+	WorkingModule.getter(&SystemWork::getWorkingModule);
+	WorkingModule.setter(&SystemWork::setWorkingModule);
+
+	FunctionStatus.setContainer(this);
+	FunctionStatus.getter(&SystemWork::getFunctionStatus);
+	FunctionStatus.setter(&SystemWork::setFunctionStatus);
+}
+
+SystemWork::SystemWork(float dt,float kp,float ki,float kd,float i_lim,float out_lim):Vehicle(dt,kp,ki,kd,i_lim,out_lim)
+{
+	// TODO Auto-generated constructor stub
+	_system_working_state = Debug;
+
+	////// System Status //////
+	WorkingModule.setContainer(this);
+	WorkingModule.getter(&SystemWork::getWorkingModule);
+	WorkingModule.setter(&SystemWork::setWorkingModule);
+
+	FunctionStatus.setContainer(this);
+	FunctionStatus.getter(&SystemWork::getFunctionStatus);
+	FunctionStatus.setter(&SystemWork::setFunctionStatus);
+}
+
+SystemWork::SystemWork(float dt,float kp,float ki,float kd,float i_lim,float out_lim,float threshold):Vehicle(dt,kp,ki,kd,i_lim,out_lim,threshold)
+{
+	// TODO Auto-generated constructor stub
+	_system_working_state = Debug;
+
+	////// System Status //////
+	WorkingModule.setContainer(this);
+	WorkingModule.getter(&SystemWork::getWorkingModule);
+	WorkingModule.setter(&SystemWork::setWorkingModule);
+
+	FunctionStatus.setContainer(this);
+	FunctionStatus.getter(&SystemWork::getFunctionStatus);
+	FunctionStatus.setter(&SystemWork::setFunctionStatus);
+}
+
+SystemWork::~SystemWork() {
+	// TODO Auto-generated destructor stub
+}
+
+uint8_t SystemWork::getWorkingModule()
+{
+	return _working_module;
+}
+void SystemWork::setWorkingModule(uint8_t value)
+{
+	_working_module = value;
+}
+
+uint8_t SystemWork::getFunctionStatus()
+{
+	return _function_status;
+}
+void SystemWork::setFunctionStatus(uint8_t value)
+{
+	_function_status = value;
+}
+
+
+void SystemWork::SystemWorkState(void)
+{
+	switch((WorkingState)_working_module)
+	{
+	case Debug:
+		DubugStataMahine();
+		break;
+
+	case Calibration:
+
+		break;
+
+	case Test:
+
+		break;
+
+	case Normal:
+
+		break;
+
+	default:
+
+		break;
+	}
+}
+
+void SystemWork::DubugStataMahine(void)
+{
+//	VehicleContorl();//vehicle control commond
+	SteeringAngleControl(0.02);// steering angle control
+
+	switch((DubugState)_function_status)
+	{
+	case DirectControl:
+
+		break;
+
+	case SpeedControl:
+		// VehicleSpeedControl(pidUpdate(VehicleSpeed));
+		VehicleSpeedControl(pidUpdateIntegralSeparation(VehicleSpeed));
+		break;
+
+	case ChangAnControl:
+		if(VehicleSpeedControlEnable)
+		{
+			// VehicleSpeedControl(pidUpdate(VehicleSpeed));
+			VehicleSpeedControl(pidUpdateIntegralSeparation(VehicleSpeed));
+		}
+		break;
+
+	case UltrasonicSR:
+
+		break;
+
+	default:
+
+		break;
+	}
+}
