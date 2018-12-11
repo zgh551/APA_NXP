afa5b1952411825f0ff357442d028bcd4224239f
朱国华 <zgh_email@163.com>
2018-12-11 15:48:29 +0800

    add gpio operation

--- main/main.cpp
+++ main/main.cpp
@@ -10,6 +10,7 @@
 #include "can.h"
 #include "pit.h"
 #include "uart.h"
+#include "gpio.h"
 #include "derivative.h" /* include peripheral declarations */
 
 
@@ -34,31 +35,52 @@ float temp;
 __attribute__ ((section(".text")))
 int main()
 {
-    xcptn_xmpl();	/* Configure and Enable Interrupts */
-    peri_clock_gating();
-    system160mhz();	/* sysclk=160MHz, dividers configured, mode trans*/
-    // Flex CAN0
-    FlexCAN0_Init();
+  xcptn_xmpl();	/* Configure and Enable Interrupts */
+  peri_clock_gating();
+  system160mhz();	/* sysclk=160MHz, dividers configured, mode trans*/
+  // Init the GPIO
+  initGPIO();
+  // Flex CAN0
+  FlexCAN0_Init();
 
-    // Flex Lin1 Uart
-    FlexLin1_Uart_Buffer_Init(80,38400);
+  // Flex Lin1 Uart
+  FlexLin1_Uart_Buffer_Init(80,38400);
 //    FlexLin1_Uart_FIFO_Init(80,19200);
 
+  // PIT0
+  PIT_0.MCR.B.MDIS = 0; /* Enable PIT module. NOTE: PIT module must be       */
+                        /* enabled BEFORE writing to it's registers.         */
+                        /* Other cores will write to PIT registers so the    */
+                        /* PIT is enabled here before starting other cores.  */
+   PIT_0.MCR.B.FRZ = 1;  /* Freeze PIT timers in debug mode */
+   PIT0_init(800000);
+   /* timeout= 0.8M  PITclks x 4 sysclks/1 PITclk x 1 sec/160Msysck */
+   /*        = 0.8M x 4 / 160M = 3.2/160 = 0.02 sec.  */
+   PIT_0.MCR.B.FRZ = 0; //Unfreeze timers
 
-    // PIT0
-    PIT_0.MCR.B.MDIS = 0; /* Enable PIT module. NOTE: PIT module must be       */
-                          /* enabled BEFORE writing to it's registers.         */
-                          /* Other cores will write to PIT registers so the    */
-                          /* PIT is enabled here before starting other cores.  */
-	PIT_0.MCR.B.FRZ = 1;  /* Freeze PIT timers in debug mode */
-	PIT0_init(800000);
-    /* timeout= 0.8M  PITclks x 4 sysclks/1 PITclk x 1 sec/160Msysck */
-    /*        = 0.8M x 4 / 160M = 3.2/160 = 0.02 sec.  */
-    PIT_0.MCR.B.FRZ = 0; //Unfreeze timers
+   LED_RED = 1;
+   LED_GREEN = 1;
+   LED_BLUE = 1;
+   LED_STA = 0;
+   /* Loop forever */
+   for(;;)
+   {
+	   if(m_Terminal_CA.APA_ControlFeedback == 1)
+	   {
+		   LED_RED = 1;
+		   LED_GREEN = 0;
+		   LED_BLUE = 1;
 
-    /* Loop forever */
-	for(;;)
-	{
+		   LED_STA = 1;
+	   }
+	   else
+	   {
+		   LED_RED = 0;
+		   LED_GREEN = 1;
+		   LED_BLUE = 1;
+
+		   LED_STA = 0;
+	   }
 //		if(TerminalSendFlag)
 //		{
 //			m_Terminal_CA.TerminalControlCommandSend();
@@ -71,7 +93,7 @@ int main()
 //			m_Terminal_CA.TerminalControlAckSend();
 //			m_Terminal_CA.AckValid = 0;
 //		}
-	}
+  }
 }
 
 void peri_clock_gating(void)
@@ -111,6 +133,18 @@ void FlexCAN0_Isr(void)
 	}
 }
 
+void FlexCAN1_Isr(void)
+{
+	if(CAN_1.IFLAG1.B.BUF31TO8I & 0x000001)
+	{
+		m_Terminal_CA.VehicleInformation(CAN_1.MB[8].ID.B.ID_STD,CAN_1.MB[8].DATA.B);
+		/* release the internal lock for all Rx MBs
+		 * by reading the TIMER */
+		uint32_t temp = CAN_1.TIMER.R;
+		CAN_1.IFLAG1.R = 0x00000100;
+	}
+}
+
 void FlexLin1_Uart_Isr(void)
 {
 	m_Terminal_CA.TerminalControlCommandReceive(LINFlexD_1.BDRM.B.DATA4);
@@ -119,4 +153,3 @@ void FlexLin1_Uart_Isr(void)
 #ifdef __cplusplus
 }
 #endif
-
