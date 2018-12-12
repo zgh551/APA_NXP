a973ecb540476f1556c6aeab0413e7e993ce8996
朱国华 <zgh_email@163.com>
2018-12-12 09:04:02 +0800

    恢复发送命令

--- Module/src/SystemWork.cpp
+++ Module/src/SystemWork.cpp
@@ -110,7 +110,7 @@ void SystemWork::SystemWorkState(void)
 
 void SystemWork::DubugStataMahine(void)
 {
-	// VehicleContorl();//vehicle control commond
+	 VehicleContorl();//vehicle control commond
 	// SteeringAngleControlStateMachine();// steering angle active
 	SteeringAngleControlStateMachineDelay();// steering angle active
 	// SteeringAngleControlStateMachineGpio();// steering angle active
