5cb30b9285d2e97754ca691e74648b9925579f42
朱国华 <zgh_email@163.com>
2018-12-15 12:26:06 +0800

    test ultrasonic

--- main/main.cpp
+++ main/main.cpp
@@ -6,12 +6,15 @@
 #include "derivative.h" /* include peripheral declarations */
 #include "mode_entry.h"
 //#include "SystemWork.h"
-#include "PathPlanning.h"
+#include "Ultrasonic.h"
+//#include "PathPlanning.h"
 #include "Terminal.h"
+
 #include "can.h"
 #include "pit.h"
 #include "uart.h"
 #include "gpio.h"
+#include "linflexd.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -22,6 +25,10 @@ extern void xcptn_xmpl(void);
 #endif
 
 Terminal m_Terminal_CA = Terminal(0.02,3.5,0.1,0.1,0.3,1,0.1);
+Ultrasonic m_Ultrasonic;
+LIN_STP318_Packet m_LIN_STP318_Packet;
+LIN_Packet m_LIN_Packet;
+
 vuint8_t cnt;
 bool TerminalSendFlag = false;
 float temp;
@@ -47,9 +54,11 @@ int main()
   FlexCAN2_Init();
 
   // Init Flex Lin1 Uart used to communication with terminal pc
-  FlexLin1_Uart_Buffer_Init(80,38400);
+
+//  FlexLin1_Uart_Buffer_Init(100,38400);
 //    FlexLin1_Uart_FIFO_Init(80,19200);
 
+  InitLINFlexD0(100,19200);
   // Init the PIT0
   PIT_0.MCR.B.MDIS = 0; /* Enable PIT module. NOTE: PIT module must be       */
                         /* enabled BEFORE writing to it's registers.         */
@@ -62,6 +71,36 @@ int main()
    PIT_0.MCR.B.FRZ = 0; //Unfreeze timers
 
    /* Loop forever */
+//   m_LIN_Packet.id = 0x35;
+//   m_LIN_Packet.length = 8;
+//
+//   m_LIN_Packet.BufferData.B.Data0 = 0x1;
+//   m_LIN_Packet.BufferData.B.Data1 = 0x2;
+//   m_LIN_Packet.BufferData.B.Data2 = 0x3;
+//   m_LIN_Packet.BufferData.B.Data3 = 0x4;
+//   m_LIN_Packet.BufferData.B.Data4 = 0x5;
+//   m_LIN_Packet.BufferData.B.Data5 = 0x6;
+//   m_LIN_Packet.BufferData.B.Data6 = 0x7;
+//   m_LIN_Packet.BufferData.B.Data7 = 0x8;
+
+//   m_LIN_Packet.BufferData.L.B.Data0 = 0x1;
+//   m_LIN_Packet.BufferData.L.B.Data1 = 0x1;
+//   m_LIN_Packet.BufferData.L.B.Data2 = 0x1;
+//   m_LIN_Packet.BufferData.L.B.Data3 = 0x1;
+//   m_LIN_Packet.BufferData.M.B.Data4 = 0x1;
+//   m_LIN_Packet.BufferData.M.B.Data5 = 0x1;
+//   m_LIN_Packet.BufferData.M.B.Data6 = 0x1;
+
+//   m_LIN_Packet.BufferData.Data[0]=0x1;
+//   m_LIN_Packet.BufferData.Data[1]=0x2;
+//   m_LIN_Packet.BufferData.Data[2]=0x3;
+//   m_LIN_Packet.BufferData.Data[3]=0x4;
+//   m_LIN_Packet.BufferData.Data[4]=0x5;
+//   m_LIN_Packet.BufferData.Data[5]=0x6;
+//   m_LIN_Packet.BufferData.Data[6]=0x7;
+//   m_LIN_Packet.BufferData.Data[7]=0x8;
+
+//   LIN0_TransmitFrame(m_LIN_Packet);
    for(;;)
    {
 //     SYSTEM_LED = LED_ON;
@@ -85,11 +124,16 @@ extern "C" {
 #endif
 void PIT0_isr(void)
 {
-	m_Terminal_CA.SystemWorkState();
+//	m_Terminal_CA.SystemWorkState();
 	cnt = (cnt + 1) % 5;
 	if(cnt == 0)
 	{
 		TerminalSendFlag = true;
+		m_Ultrasonic.InitSensing_STP318(1, 1);
+	}
+	if(cnt == 3)
+	{
+		m_LIN_STP318_Packet = m_Ultrasonic.ReadData_STP318(0xCf);
 	}
 	PIT_0.TIMER[0].TFLG.R |= 1;  /* Clear interrupt flag. w1c */
 }
@@ -105,7 +149,6 @@ void FlexCAN0_Isr(void)
 		CAN_0.IFLAG1.R = 0x00000100;
 	}
 }
-
 void FlexCAN1_Isr(void)
 {
 	if(CAN_1.IFLAG1.B.BUF31TO8I & 0x000001)
@@ -117,7 +160,6 @@ void FlexCAN1_Isr(void)
 		CAN_1.IFLAG1.R = 0x00000100;
 	}
 }
-
 void FlexCAN2_Isr(void)
 {
 	if(CAN_2.IFLAG1.B.BUF31TO8I & 0x000001)
