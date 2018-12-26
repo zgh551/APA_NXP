303df4fd6652cbfbdd2d318f1b86db1990ed6dfe
朱国华 <zgh_email@163.com>
2018-12-26 14:01:14 +0800

    revise the periperty init function and test the ultrasonic module

--- main/main.cpp
+++ main/main.cpp
@@ -26,69 +26,37 @@ extern void xcptn_xmpl(void);
 
 Terminal m_Terminal_CA = Terminal(0.02,3.5,0.1,0.1,0.3,1,0.1);
 Ultrasonic m_Ultrasonic;
-LIN_STP318_Packet m_LIN_STP318_Packet;
-LIN_Packet m_LIN_Packet;
 
 vuint8_t cnt;
 bool TerminalSendFlag = false;
 float temp;
 
-uint8_t d0,d1,d2;
 __attribute__ ((section(".text")))
 int main()
 {
+		/* Configure and Enable Interrupts */
+		xcptn_xmpl();
 
-	/* Configure and Enable Interrupts */
-	xcptn_xmpl();
+		/* Sysclk = 200MHz, dividers configured, mode trans*/
+		SystemClockConfigure();
 
-	/* Sysclk = 200MHz, dividers configured, mode trans*/
-	SystemClockConfigure();
+		/* Init the GPIO */
+		initGPIO();
 
+		/* Init CANFlex Module */
+		CAN_Configure();
 
-    /* Init the GPIO */
-	initGPIO();
+		/* Init LinFlexD Module */
+		LINFlexD_Configure();
 
-	/* Init CANFlex Module */
-	FlexCAN0_Init();
-	FlexCAN1_Init();
-	FlexCAN2_Init();
+		/* Init PIT Module */
+		PIT_Configure();
 
-/// Init Flex Lin1 Uart used to communication with terminal pc
-//  FlexLin1_Uart_Buffer_Init(100,38400);
+		/* Loop forever */
+		for(;;)
+		{
 
-//	InitLINFlexD0(100,19200);
-
-	InitLINFlexD0_DMA(100,19200);
-	FlexLin0_DMA_TX_M2S_Init();
-	FlexLin0_DMA_TX_S2M_Init();
-	FlexLin0_DMA_RX_S2M_Init();
-/// Init the PIT0
-	PIT_0.MCR.B.MDIS = 0; /* Enable PIT module. NOTE: PIT module must be       */
-                        /* enabled BEFORE writing to it's registers.         */
-                        /* Other cores will write to PIT registers so the    */
-                        /* PIT is enabled here before starting other cores.  */
-	PIT_0.MCR.B.FRZ = 1;  /* Freeze PIT timers in debug mode */
-	PIT0_init(1000000);
-	/* timeout= 0.8M  PITclks x 4 sysclks/1 PITclk x 1 sec/160Msysck */
-	/*        = 0.8M x 4 / 160M = 3.2/160 = 0.02 sec.  */
-	PIT_0.MCR.B.FRZ = 0; //Unfreeze timers
-
-   /* Loop forever */
-   for(;;)
-   {
-//		if(TerminalSendFlag)
-//		{
-//			m_Terminal_CA.TerminalControlCommandSend();
-//			m_Terminal_CA.TerminalControlSpeedSend();
-//			m_Terminal_CA.TerminalSystemStateSend();
-//			TerminalSendFlag = false;
-//		}
-//		if(m_Terminal_CA.AckValid == 0x5A)
-//		{
-//			m_Terminal_CA.TerminalControlAckSend();
-//			m_Terminal_CA.AckValid = 0;
-//		}
-  }
+		}
 }
 
 #ifdef __cplusplus
@@ -96,21 +64,23 @@ extern "C" {
 #endif
 void PIT0_isr(void)
 {
-//	m_Terminal_CA.SystemWorkState();
-	SYSTEM_LED = ~SYSTEM_LED;
-	cnt = (cnt + 1) % 5;
-	if(cnt == 1)
+	m_Ultrasonic.UltrasonicScheduleStatusMachine_V2();
+	m_Ultrasonic.ScheduleTimeCnt = (m_Ultrasonic.ScheduleTimeCnt + 1) % 26;
+	if(m_Ultrasonic.ScheduleTimeCnt == 0)
 	{
-		m_Ultrasonic.InitUltrasonicSensor(0);
+		SYSTEM_LED = ~SYSTEM_LED;
 	}
-	if(cnt == 2)
-	{
-//		m_Ultrasonic.ReadSensing_STP318(LIN0_ReceiveFrame_DMA,0xCf);
-		m_Ultrasonic.ReadUltrasonicSensor(0);
-//		d0 = m_Ultrasonic.UltrasonicDatas[1].BDRL.B.DATA0;
-//		d1 = m_Ultrasonic.UltrasonicDatas[1].BDRL.B.DATA1;
-//		d2 = m_Ultrasonic.UltrasonicDatas[1].BDRL.B.DATA2;
-	}
+
+//cnt = (cnt + 1) % 20;
+//if(cnt == 1)
+//{
+//	m_Ultrasonic.InitUltrasonicSensor(7);
+//}
+//else if(cnt == 5)
+//{
+//	m_Ultrasonic.ReadUltrasonicSensor(7);
+//}
+
 	PIT_0.TIMER[0].TFLG.R |= 1;  /* Clear interrupt flag. w1c */
 }
 
@@ -146,14 +116,6 @@ void FlexCAN2_Isr(void)
 		uint32_t temp = CAN_2.TIMER.R;
 		CAN_2.IFLAG1.R = 0x00000100;
 	}
-}
-
-
-
-void FlexLin1_Uart_Isr(void)
-{
-	m_Terminal_CA.TerminalControlCommandReceive(LINFlexD_1.BDRM.B.DATA4);
-	LINFlexD_1.UARTSR.B.DRFRFE = 1;
 }
 #ifdef __cplusplus
 }
