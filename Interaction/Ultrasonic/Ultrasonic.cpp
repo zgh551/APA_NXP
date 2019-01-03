4da131f39e0509c3cfd0f8e9abd63da8755ca830
朱国华 <zgh_email@163.com>
2019-01-03 09:33:37 +0800

    add ultrasonic data pretreagtment as Ultrasonic_Packet output

--- Interaction/Ultrasonic/Ultrasonic.cpp
+++ Interaction/Ultrasonic/Ultrasonic.cpp
@@ -9,33 +9,38 @@
 
 Ultrasonic::Ultrasonic() {
 	// TODO Auto-generated constructor stub
-	_schedule_time_cnt = 0;
+	Init();
 
-		ScheduleTimeCnt.setContainer(this);
-		ScheduleTimeCnt.getter(&Ultrasonic::getScheduleTimeCnt);
-		ScheduleTimeCnt.setter(&Ultrasonic::setScheduleTimeCnt);
+	ScheduleTimeCnt.setContainer(this);
+	ScheduleTimeCnt.getter(&Ultrasonic::getScheduleTimeCnt);
+	ScheduleTimeCnt.setter(&Ultrasonic::setScheduleTimeCnt);
 
-		UltrasonicDatas.setContainer(this);
-		UltrasonicDatas.getter(&Ultrasonic::getUltrasonicDatas);
+	SystemTime.setContainer(this);
+	SystemTime.getter(&Ultrasonic::getSystemTime);
+	SystemTime.setter(&Ultrasonic::setSystemTime);
+
+	UltrasonicPacket.setContainer(this);
+	UltrasonicPacket.getter(&Ultrasonic::getUltrasonicPacket);
 }
 
 Ultrasonic::~Ultrasonic() {
 	// TODO Auto-generated destructor stub
 }
 
-uint8_t Ultrasonic::getScheduleTimeCnt()
+void Ultrasonic::Init(void)
 {
-		return _schedule_time_cnt;
-}
-void  Ultrasonic::setScheduleTimeCnt(uint8_t value)
-{
-		_schedule_time_cnt = value;
+	_system_time = 0;
+	_schedule_time_cnt = 0;
+	_read_stage = 0;
 }
 
-LIN_RAM* Ultrasonic::getUltrasonicDatas()
-{
-	return _ultrasonic_datas;
-}
+uint8_t Ultrasonic::getScheduleTimeCnt()             {return _schedule_time_cnt ;}
+void    Ultrasonic::setScheduleTimeCnt(uint8_t value){_schedule_time_cnt = value;}
+
+uint32_t Ultrasonic::getSystemTime()              {return _system_time ;}
+void     Ultrasonic::setSystemTime(uint32_t value){_system_time = value;}
+
+Ultrasonic_Packet* Ultrasonic::getUltrasonicPacket(){return _ultrasonic_packet;}
 
 void Ultrasonic::InitSensing_STP318(uint8_t tx,uint8_t rx,void (*TransmitFrame)(LIN_RAM))
 {
@@ -247,84 +252,226 @@ void Ultrasonic::UltrasonicScheduleStatusMachine_V2(void)
 		case 0:
 			InitUltrasonicSensor(8);
 			InitUltrasonicSensor(11);
+			_ultrasonic_packet[8].Time_Tx  = _system_time;
+			_ultrasonic_packet[11].Time_Tx = _system_time;
 			break;
 
 		case 1:
 			InitUltrasonicSensor(1);
 			InitUltrasonicSensor(7);
+			_ultrasonic_packet[1].Time_Tx = _system_time;
+			_ultrasonic_packet[7].Time_Tx = _system_time;
 			break;
 
 		case 2:
 			InitUltrasonicSensor(9);
 			InitUltrasonicSensor(10);
+			_ultrasonic_packet[9].Time_Tx  = _system_time;
+			_ultrasonic_packet[10].Time_Tx = _system_time;
 			break;
 
 		case 6:
 			ReadUltrasonicSensor(1);
 			ReadUltrasonicSensor(7);
+			_read_stage = 0;
 			break;
 
 		case 7:
 			InitUltrasonicSensor(3);
 			InitUltrasonicSensor(5);
+			_ultrasonic_packet[3].Time_Tx = _system_time;
+			_ultrasonic_packet[5].Time_Tx = _system_time;
 			break;
 
 		case 8:
 			ReadUltrasonicSensor(8);
 			ReadUltrasonicSensor(11);
+			_read_stage = 1;
 			break;
 
 		case 10:
 			ReadUltrasonicSensor(9);
 			ReadUltrasonicSensor(10);
+			_read_stage = 2;
 			break;
 
 		case 12:
 			ReadUltrasonicSensor(3);
 			ReadUltrasonicSensor(5);
+			_read_stage = 3;
 			break;
 
 		case 13:
 			InitUltrasonicSensor(8);
 			InitUltrasonicSensor(11);
+			_ultrasonic_packet[8].Time_Tx  = _system_time;
+			_ultrasonic_packet[11].Time_Tx = _system_time;
 			break;
 
 		case 14:
 			InitUltrasonicSensor(0);
 			InitUltrasonicSensor(6);
+			_ultrasonic_packet[0].Time_Tx = _system_time;
+			_ultrasonic_packet[6].Time_Tx = _system_time;
 			break;
 
 		case 15:
 			InitUltrasonicSensor(9);
 			InitUltrasonicSensor(10);
+			_ultrasonic_packet[9].Time_Tx = _system_time;
+			_ultrasonic_packet[10].Time_Tx = _system_time;
 			break;
 
 		case 19:
 			ReadUltrasonicSensor(0);
 			ReadUltrasonicSensor(6);
+			_read_stage = 4;
 			break;
 
 		case 20:
 			InitUltrasonicSensor(2);
 			InitUltrasonicSensor(4);
+			_ultrasonic_packet[2].Time_Tx = _system_time;
+			_ultrasonic_packet[4].Time_Tx = _system_time;
 			break;
 
 		case 21:
 			ReadUltrasonicSensor(8);
 			ReadUltrasonicSensor(11);
+			_read_stage = 5;
 			break;
 
 		case 23:
 			ReadUltrasonicSensor(9);
 			ReadUltrasonicSensor(10);
+			_read_stage = 6;
 			break;
 
 		case 25:
 			ReadUltrasonicSensor(2);
 			ReadUltrasonicSensor(4);
+			_read_stage = 7;
 			break;
 
 		default:
 			break;
     }
+}
+
+float Ultrasonic::Compensation(float temp)
+{
+	return (331.5 + 0.60714 * temp) * 0.0000005; // (m/us)/2
+}
+
+void Ultrasonic::Update(uint8_t id,float t)
+{
+	switch(_read_stage)
+	{
+		case 0:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[1].Distance1 = _ultrasonic_datas[1].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[1].status = _ultrasonic_datas[1].STP318.Status;
+				 _ultrasonic_packet[1].Time_Ms = _ultrasonic_packet[1].Time_Tx * 5 + _ultrasonic_datas[1].STP318.TOF * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[7].Distance1 = _ultrasonic_datas[7].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[7].status = _ultrasonic_datas[7].STP318.Status;
+				 _ultrasonic_packet[7].Time_Ms = _ultrasonic_packet[7].Time_Tx * 5 + _ultrasonic_datas[7].STP318.TOF * 0.0005;
+			}
+			break;
+
+		case 1:
+		case 5:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[8].Distance1 = _ultrasonic_datas[8].STP313.TOF1 * Compensation(t);
+				 _ultrasonic_packet[8].Distance2 = _ultrasonic_datas[8].STP313.TOF2 * Compensation(t);
+				 _ultrasonic_packet[8].Level = _ultrasonic_datas[8].STP313.Level * LEVEL_RATIO;
+				 _ultrasonic_packet[8].Width = _ultrasonic_datas[8].STP313.Width * WIDTH_RATIO;
+				 _ultrasonic_packet[8].status = _ultrasonic_datas[8].STP313.Status;
+				 _ultrasonic_packet[8].Time_Ms = _ultrasonic_packet[8].Time_Tx * 5 + _ultrasonic_datas[8].STP313.TOF1 * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[11].Distance1 = _ultrasonic_datas[11].STP313.TOF1 * Compensation(t);
+				 _ultrasonic_packet[11].Distance2 = _ultrasonic_datas[11].STP313.TOF2 * Compensation(t);
+				 _ultrasonic_packet[11].Level = _ultrasonic_datas[11].STP313.Level * LEVEL_RATIO;
+				 _ultrasonic_packet[11].Width = _ultrasonic_datas[11].STP313.Width * WIDTH_RATIO;
+				 _ultrasonic_packet[11].status = _ultrasonic_datas[11].STP313.Status;
+				 _ultrasonic_packet[11].Time_Ms = _ultrasonic_packet[11].Time_Tx * 5 + _ultrasonic_datas[11].STP313.TOF1 * 0.0005;
+			}
+			break;
+
+		case 2:
+		case 6:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[9].Distance1 = _ultrasonic_datas[9].STP313.TOF1 * Compensation(t);
+				 _ultrasonic_packet[9].Distance2 = _ultrasonic_datas[9].STP313.TOF2 * Compensation(t);
+				 _ultrasonic_packet[9].Level = _ultrasonic_datas[9].STP313.Level * LEVEL_RATIO;
+				 _ultrasonic_packet[9].Width = _ultrasonic_datas[9].STP313.Width * WIDTH_RATIO;
+				 _ultrasonic_packet[9].status = _ultrasonic_datas[9].STP313.Status;
+				 _ultrasonic_packet[9].Time_Ms = _ultrasonic_packet[9].Time_Tx * 5 + _ultrasonic_datas[9].STP313.TOF1 * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[10].Distance1 = _ultrasonic_datas[10].STP313.TOF1 * Compensation(t);
+				 _ultrasonic_packet[10].Distance2 = _ultrasonic_datas[10].STP313.TOF2 * Compensation(t);
+				 _ultrasonic_packet[10].Level     = _ultrasonic_datas[10].STP313.Level * LEVEL_RATIO;
+				 _ultrasonic_packet[10].Width     = _ultrasonic_datas[10].STP313.Width * WIDTH_RATIO;
+				 _ultrasonic_packet[10].status    = _ultrasonic_datas[10].STP313.Status;
+				 _ultrasonic_packet[10].Time_Ms   = _ultrasonic_packet[10].Time_Tx * 5 + _ultrasonic_datas[10].STP313.TOF1 * 0.0005;
+			}
+			break;
+
+		case 3:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[3].Distance1 = _ultrasonic_datas[3].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[3].status    = _ultrasonic_datas[3].STP318.Status;
+				 _ultrasonic_packet[3].Time_Ms   = _ultrasonic_packet[3].Time_Tx * 5 + _ultrasonic_datas[3].STP318.TOF * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[5].Distance1 = _ultrasonic_datas[5].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[5].status    = _ultrasonic_datas[5].STP318.Status;
+				 _ultrasonic_packet[5].Time_Ms   = _ultrasonic_packet[5].Time_Tx * 5 + _ultrasonic_datas[5].STP318.TOF * 0.0005;
+			}
+			break;
+
+		case 4:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[0].Distance1 = _ultrasonic_datas[0].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[0].status    = _ultrasonic_datas[0].STP318.Status;
+				 _ultrasonic_packet[0].Time_Ms   = _ultrasonic_packet[0].Time_Tx * 5 + _ultrasonic_datas[0].STP318.TOF * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[6].Distance1 = _ultrasonic_datas[6].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[6].status    = _ultrasonic_datas[6].STP318.Status;
+				 _ultrasonic_packet[6].Time_Ms   = _ultrasonic_packet[6].Time_Tx * 5 + _ultrasonic_datas[6].STP318.TOF * 0.0005;
+			}
+			break;
+
+		case 7:
+			if(id == 0)
+			{
+				 _ultrasonic_packet[2].Distance1 = _ultrasonic_datas[2].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[2].status    = _ultrasonic_datas[2].STP318.Status;
+				 _ultrasonic_packet[2].Time_Ms   = _ultrasonic_packet[2].Time_Tx * 5 + _ultrasonic_datas[2].STP318.TOF * 0.0005;
+			}
+			else
+			{
+				 _ultrasonic_packet[4].Distance1 = _ultrasonic_datas[4].STP318.TOF * Compensation(t);
+				 _ultrasonic_packet[4].status    = _ultrasonic_datas[4].STP318.Status;
+				 _ultrasonic_packet[4].Time_Ms   = _ultrasonic_packet[4].Time_Tx * 5 + _ultrasonic_datas[4].STP318.TOF * 0.0005;
+			}
+			break;
+
+		default:
+			break;
+	}
 }
--- Interaction/Ultrasonic/Ultrasonic.h
+++ Interaction/Ultrasonic/Ultrasonic.h
@@ -8,11 +8,14 @@
 #ifndef ULTRASONIC_H_
 #define ULTRASONIC_H_
 
-#include "Property.h"
-#include "linflexd.h"
+#include "derivative.h"
+#include "property.h"
 
 #define FRONT_ULTRASONIC_ENABLE
 #define REAR_ULTRASONIC_ENABLE
+
+#define LEVEL_RATIO    (0.01294117647058823529411764705882)
+#define WIDTH_RATIO    (16)
 /*** LIN Device Data Struct ***/
 typedef struct _LIN_STP318_Packet
 {
@@ -29,10 +32,24 @@ typedef struct _LIN_STP313_Packet
 	uint8_t status;
 }LIN_STP313_Packet;
 
+typedef struct _Ultrasonic_Packet
+{
+	float Distance1;
+	float Distance2;
+	float Level;
+	float Width;
+	float Time_Ms;
+	uint8_t status;
+	uint32_t Time_Tx;
+
+}Ultrasonic_Packet;
+
 class Ultrasonic {
 public:
 	Ultrasonic();
 	virtual ~Ultrasonic();
+
+	void Init(void);
 
 	void InitSensing_STP318(uint8_t tx,uint8_t rx,void (*TransmitFrame)(LIN_RAM));
 	void InitSensing_STP313(uint8_t tx_rx,void (*TransmitFrame)(LIN_RAM));
@@ -46,18 +63,30 @@ public:
 	void UltrasonicScheduleStatusMachine(void);
 	void UltrasonicScheduleStatusMachine_V2(void);
 
+	float Compensation(float temp);
+
+	void Update(uint8_t id,float t);
+
 	/// Property
 	uint8_t getScheduleTimeCnt();
 	void    setScheduleTimeCnt(uint8_t value);
 	Property<Ultrasonic,uint8_t,READ_WRITE> ScheduleTimeCnt;
 
-	LIN_RAM* getUltrasonicDatas();
-	Property<Ultrasonic,LIN_RAM*,READ_ONLY> UltrasonicDatas;
+	uint32_t getSystemTime();
+	void     setSystemTime(uint32_t value);
+	Property<Ultrasonic,uint32_t,READ_WRITE> SystemTime;
+
+	Ultrasonic_Packet* getUltrasonicPacket();
+	Property<Ultrasonic,Ultrasonic_Packet*,READ_ONLY> UltrasonicPacket;
 
 private:
-	uint8_t _schedule_time_cnt;
+	uint32_t _system_time;
+	uint8_t  _schedule_time_cnt;
+	uint8_t  _read_stage;
 
 	LIN_RAM _ultrasonic_datas[12];
+
+	Ultrasonic_Packet _ultrasonic_packet[12];
 };
 
 #endif /* ULTRASONIC_H_ */
