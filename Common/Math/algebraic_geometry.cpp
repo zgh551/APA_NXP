09f4721ab8a5b112f90d1972d3e893423367d8c3
朱国华 <zgh_email@163.com>
2019-01-16 19:15:06 +0800

    add new file about the algebraic geometry

---
+++ Common/Math/algebraic_geometry.cpp
@@ -0,0 +1,18 @@
+/*
+ * algebraic_geometry.cpp
+ *
+ *  Created on: 2019Äê1ÔÂ16ÈÕ
+ *      Author: zhuguohua
+ */
+
+#include "algebraic_geometry.h"
+
+AlgebraicGeometry::AlgebraicGeometry() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AlgebraicGeometry::~AlgebraicGeometry() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Common/Math/algebraic_geometry.h
@@ -0,0 +1,22 @@
+/*
+ * algebraic_geometry.h
+ *
+ *  Created on: 2019Äê1ÔÂ16ÈÕ
+ *      Author: zhuguohua
+ */
+
+#ifndef MATH_ALGEBRAIC_GEOMETRY_H_
+#define MATH_ALGEBRAIC_GEOMETRY_H_
+
+#include "derivative.h"
+#include "property.h"
+#include "vector_2d.h"
+#include "math.h"
+
+class AlgebraicGeometry {
+public:
+	AlgebraicGeometry();
+	virtual ~AlgebraicGeometry();
+};
+
+#endif /* MATH_ALGEBRAIC_GEOMETRY_H_ */
