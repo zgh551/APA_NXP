418fc763dac42345d0c756e3dcf22e62a82e29ac
朱国华 <zgh_email@163.com>
2019-02-12 16:04:42 +0800

    add ultrasonic abstacle percption file

---
+++ Percaption/UltrasonicPercaption/ultrasonic_abstacle_percption.cpp
@@ -0,0 +1,18 @@
+/*
+ * ultrasonic_abstacle_percption.cpp
+ *
+ *  Created on: 2019Äê1ÔÂ29ÈÕ
+ *      Author: zhuguohua
+ */
+
+#include <ultrasonic_abstacle_percption.h>
+
+UltrasonicAbstaclePercption::UltrasonicAbstaclePercption() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UltrasonicAbstaclePercption::~UltrasonicAbstaclePercption() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Percaption/UltrasonicPercaption/ultrasonic_abstacle_percption.h
@@ -0,0 +1,20 @@
+/*
+ * ultrasonic_abstacle_percption.h
+ *
+ *  Created on: 2019Äê1ÔÂ29ÈÕ
+ *      Author: zhuguohua
+ */
+
+#ifndef ULTRASONICPERCAPTION_ULTRASONIC_ABSTACLE_PERCPTION_H_
+#define ULTRASONICPERCAPTION_ULTRASONIC_ABSTACLE_PERCPTION_H_
+
+class UltrasonicAbstaclePercption {
+public:
+	UltrasonicAbstaclePercption();
+	virtual ~UltrasonicAbstaclePercption();
+
+private:
+
+};
+
+#endif /* ULTRASONICPERCAPTION_ULTRASONIC_ABSTACLE_PERCPTION_H_ */
