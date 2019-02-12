206da06b1abe5f2d0e6d2a9e748389781cc7339c
朱国华 <zgh_email@163.com>
2019-02-12 16:04:10 +0800

    add vertical planning file

---
+++ Planning/VerticalParking/vertical_planning.cpp
@@ -0,0 +1,18 @@
+/*
+ * vertical_planning.cpp
+ *
+ *  Created on: 2019Äê2ÔÂ12ÈÕ
+ *      Author: zhuguohua
+ */
+
+#include <vertical_planning.h>
+
+VerticalPlanning::VerticalPlanning() {
+	// TODO Auto-generated constructor stub
+
+}
+
+VerticalPlanning::~VerticalPlanning() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Planning/VerticalParking/vertical_planning.h
@@ -0,0 +1,17 @@
+/*
+ * vertical_planning.h
+ *
+ *  Created on: 2019Äê2ÔÂ12ÈÕ
+ *      Author: zhuguohua
+ */
+
+#ifndef VERTICALPARKING_VERTICAL_PLANNING_H_
+#define VERTICALPARKING_VERTICAL_PLANNING_H_
+
+class VerticalPlanning {
+public:
+	VerticalPlanning();
+	virtual ~VerticalPlanning();
+};
+
+#endif /* VERTICALPARKING_VERTICAL_PLANNING_H_ */
