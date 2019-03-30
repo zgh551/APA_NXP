b33f754ad6889ff705c8b1723a88fee3084909ed
朱国华 <zgh_email@163.com>
2019-03-30 13:30:10 +0800

    添加全局配置宏开关

---
+++ Common/Configure/Configs/system_config.h
@@ -0,0 +1,18 @@
+/*
+ * system_config.h
+ *
+ *  Created on: 2019年3月30日
+ *      Author: zhuguohua
+ */
+
+#ifndef CONFIGURE_CONFIGS_SYSTEM_CONFIG_H_
+#define CONFIGURE_CONFIGS_SYSTEM_CONFIG_H_
+
+/****************System Configure******************/
+//#define CHANGAN
+#define BORUI
+#define SIMULATION 0
+
+
+
+#endif /* CONFIGURE_CONFIGS_SYSTEM_CONFIG_H_ */
