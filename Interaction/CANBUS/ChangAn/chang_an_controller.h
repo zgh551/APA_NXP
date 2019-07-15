b5f4ccbccb001195af7c6a42271622bc4809f372
朱国华 <zgh_email@163.com>
2019-07-15 14:42:45 +0800

    新的网管控制代码更新

--- Interaction/CANBUS/ChangAn/chang_an_controller.h
+++ Interaction/CANBUS/ChangAn/chang_an_controller.h
@@ -28,6 +28,12 @@ typedef enum _SteeringAngleActiveControl
 	WaitExistState
 }SteeringAngleActiveControl;
 
+typedef enum _GearActiveControlState
+{
+	WaitGearControlState = 0,
+	WaitGearFeedbackSingleState,
+	WaitGearExistState
+}GearActiveControlState;
 
 class ChangAnController : public VehicleController
 {
@@ -65,16 +71,20 @@ public:
 	void VehicleContorlStep1();
 	void VehicleContorlStep2();
 	void VehicleContorlStep3();
+
+	void VehicleContorlNew();
 	void VehicleContorl();
 
 	// Steeing angle control base on the angle speed
-	void SteeringAngleControl(float dt);
+	void SteeringAngleControl(float dt,float steer_angle);
 
 	// Steering Angle control state machine
 	void SteeringAngleControlStateMachine(uint8_t fd);
 
+	void GearControlStateMachine(uint8_t fd);
 private:
 	SteeringAngleActiveControl _steerig_angle_active_control_state;
+	GearActiveControlState     _gear_active_control_state;
 	/*** Send to Vehicle Messege ***/
 	/* Roolling Counter */
 	uint8_t _rolling_counter_torque_AEB;
