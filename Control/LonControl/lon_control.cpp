3e1af023830c64b5fe47303308b906eed69e73a2
朱国华 <zgh_email@163.com>
2019-07-31 17:30:20 +0800

    1、修改长安得控制，解决车辆换挡滑移问题； 2、东风车速控制接口

--- Control/LonControl/lon_control.cpp
+++ Control/LonControl/lon_control.cpp
@@ -1,9 +1,13 @@
-/*
- * lon_control.cpp
- *
- *  Created on: 2019年1月3日
- *      Author: zhuguohua
- */
+/*****************************************************************************/
+/* FILE NAME: lon_control.cpp                     COPYRIGHT (c) Motovis 2019 */
+/*                                                       All Rights Reserved */
+/* DESCRIPTION: this class using to interpolation data  			         */
+/*****************************************************************************/
+/* REV      AUTHOR        DATE              DESCRIPTION OF CHANGE            */
+/* ---   -----------    ----------------    ---------------------            */
+/* 1.0	 Guohua Zhu     January 3 2019      Initial Version                  */
+/* 1.0	 Guohua Zhu     July   30 2019      Add Dongfeng Control Function    */
+/*****************************************************************************/
 
 #include <lon_control.h>
 
@@ -21,6 +25,10 @@ void LonControl::Init()
 	_min_position = MIN_POSITION;// 速度控制下限点
 	_max_velocity = MAX_VELOCITY;// 直线段的速度
 	_min_velocity = MIN_VELOCITY;// 曲线段的速度
+
+	_throttle_lowerbound = 50;//Nm
+	_brake_lowerbound = 0;// m/s2
+
 }
 
 //根据距离规划速度
@@ -56,45 +64,95 @@ void LonControl::Proc(MessageManager *msg,VehicleController *ctl,PID *velocity_p
 {
 	if(ctl->VelocityEnable)
 	{
-		float v = (msg->WheelSpeedRearLeft + msg->WheelSpeedRearRight) * 0.5;
-		velocity_pid->Desired = ctl->Velocity;
-//		pid->Desired = VelocityControl(ctl->Velocity,ctl->Distance);
-//		ctl->Acceleration = velocity_pid->pidUpdateIntegralSeparation(v);
-		ctl->Torque = velocity_pid->pidUpdate(v);
+		_vehicle_velocity = (msg->WheelSpeedRearLeft + msg->WheelSpeedRearRight) * 0.5;
+//		velocity_pid->Desired = ctl->Velocity;
+		velocity_pid->Desired = VelocityControl(ctl->Velocity,ctl->Distance);
+		ctl->Acceleration = velocity_pid->pidUpdateIntegralSeparation(_vehicle_velocity);
+		if((ctl->getAcceleration() < 1.0e-6) && (velocity_pid->Desired < 1.0e-6))
+		{
+			ctl->Acceleration = -0.5;
+		}
 	}
 }
 
-void LonControl::Proc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid,PID *acc_pid)
+void LonControl::VelocityProc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid)
 {
 	if(ctl->VelocityEnable)
 	{
-		float v = (msg->WheelSpeedRearLeft + msg->WheelSpeedRearRight) * 0.5;
+		_vehicle_velocity = (msg->WheelSpeedRearLeft + msg->WheelSpeedRearRight) * 0.5;
 		velocity_pid->Desired = ctl->Velocity;
-		acc_pid->Desired = velocity_pid->pidUpdateIntegralSeparation(v);
-		if(acc_pid->Desired > 0)
-		{
-			if(msg->Gear == Drive)
-			{
-				ctl->Acceleration = acc_pid->pidUpdateIntegralSeparation(msg->LonAcc);
-			}
-			else if(msg->Gear == Reverse)
-			{
-				ctl->Acceleration = acc_pid->pidUpdateIntegralSeparation(-msg->LonAcc);
-			}
-		}
-		else
-		{
-			ctl->Acceleration = acc_pid->Desired;
-		}
-
+		ctl->TargetAcceleration = velocity_pid->pidUpdateIntegralSeparation(_vehicle_velocity);
 	}
 }
 
 void LonControl::AccProc(MessageManager *msg,VehicleController *ctl,PID *acc_pid)
 {
-	if(ctl->AccelerationEnable)
+	if(ctl->TorqueEnable)
 	{
-		acc_pid->Desired = ctl->TargetAcceleration;
-		ctl->Acceleration = acc_pid->pidUpdateIntegralSeparation(msg->LonAcc);
+		if(ctl->TargetAcceleration < 0)
+		{
+			ctl->Torque = 0;
+			ctl->Acceleration = ctl->TargetAcceleration;
+			ctl->AccelerationEnable = 1;
+		}
+		else
+		{
+			acc_pid->Desired = ctl->TargetAcceleration;
+			if(msg->Gear == Drive)
+			{
+				ctl->Torque = acc_pid->pidUpdateIntegralSeparation(msg->LonAcc);
+			}
+			else if(msg->Gear == Reverse)
+			{
+				ctl->Torque = acc_pid->pidUpdateIntegralSeparation(-msg->LonAcc);
+			}
+			ctl->Acceleration = 0;
+			ctl->AccelerationEnable = 0;
+		}
+	}
+}
+
+void LonControl::VelocityLookupProc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid)
+{
+	if(ctl->VelocityEnable)
+	{
+		_vehicle_velocity = (msg->WheelSpeedRearLeft + msg->WheelSpeedRearRight) * 0.5;
+//		velocity_pid->Desired = ctl->Velocity;
+		velocity_pid->Desired   = VelocityControl(ctl->Velocity,ctl->Distance);
+		ctl->TargetAcceleration = velocity_pid->pidUpdateIntegralSeparation(_vehicle_velocity);
+
+		if(ctl->TargetAcceleration >= 0)
+		{
+			ctl->TorqueEnable       = 1;
+			ctl->AccelerationEnable = 0;
+			_calibration_value = _lon_Interpolation.Interpolation2D(ctl->TargetAcceleration, _vehicle_velocity,
+																	_lon_VehilceConfig.AccelerateTable, _lon_VehilceConfig.AccNum,
+																	_lon_VehilceConfig.VelocityTable, _lon_VehilceConfig.VlcNum,
+																	_lon_VehilceConfig.TorqueTable);
+			if(_calibration_value >= 0)
+			{
+				ctl->Torque = fmax(_calibration_value,_throttle_lowerbound);//添加最小死区的判断
+			}
+			else
+			{
+				ctl->Torque = _throttle_lowerbound;
+			}
+			ctl->Acceleration = 0;
+		}
+		else
+		{
+			ctl->AccelerationEnable = 1;
+			ctl->TorqueEnable       = 1;
+			_calibration_value = ctl->TargetAcceleration;
+			if(_calibration_value >= 0)
+			{
+				ctl->Acceleration = _brake_lowerbound;
+			}
+			else
+			{
+				ctl->Acceleration = _calibration_value;
+			}
+			ctl->Torque = 0;
+		}
 	}
 }
--- Control/LonControl/lon_control.h
+++ Control/LonControl/lon_control.h
@@ -1,9 +1,13 @@
-/*
- * lon_control.h
- *
- *  Created on: 2019年1月3日
- *      Author: zhuguohua
- */
+/*****************************************************************************/
+/* FILE NAME: lon_control.h                       COPYRIGHT (c) Motovis 2019 */
+/*                                                       All Rights Reserved */
+/* DESCRIPTION: this class using to interpolation data  			         */
+/*****************************************************************************/
+/* REV      AUTHOR        DATE              DESCRIPTION OF CHANGE            */
+/* ---   -----------    ----------------    ---------------------            */
+/* 1.0	 Guohua Zhu     January 3 2019      Initial Version                  */
+/* 1.0	 Guohua Zhu     July   30 2019      Add Dongfeng Control Function    */
+/*****************************************************************************/
 
 #ifndef LONCONTROL_LON_CONTROL_H_
 #define LONCONTROL_LON_CONTROL_H_
@@ -12,11 +16,14 @@
 #include "property.h"
 #include "controller.h"
 #include "pid.h"
+#include "math.h"
+#include "vehilce_config.h"
+#include "interpolation.h"
 
 /**************************速度控制******************************/
 #define MAX_POSITION            ( 0.8 ) // 速度控制上限点
 #define MIN_POSITION            ( 0.4 ) // 速度控制下限点
-#define MAX_VELOCITY	  		( 0.5 ) // 直线段的速度
+#define MAX_VELOCITY	  		( 0.8 ) // 直线段的速度
 #define MIN_VELOCITY	      	( 0.3 ) // 曲线段的速度
 
 class LonControl:public Controller
@@ -29,15 +36,25 @@ public:
 
 	void Proc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid) override;
 
-	void Proc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid,PID *acc_pid);
+	void VelocityProc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid);
 
 	void AccProc(MessageManager *msg,VehicleController *ctl,PID *acc_pid);
+
+	void VelocityLookupProc(MessageManager *msg,VehicleController *ctl,PID *velocity_pid);
 
 	float VelocityPlanningControl(float distance);
 	float VelocityControl(float distance,float velocity);
 private:
 	float _max_position,_min_position;
 	float _max_velocity,_min_velocity;
+
+	float _vehicle_velocity;	// 车辆速度
+	float _throttle_lowerbound; // 油门最低边界
+	float _brake_lowerbound;    // 制动最低边界
+	float _calibration_value;   // 校准值
+
+	VehilceConfig _lon_VehilceConfig;
+	Interpolation _lon_Interpolation;
 };
 
 #endif /* LONCONTROL_LON_CONTROL_H_ */
