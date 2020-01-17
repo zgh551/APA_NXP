035ac4ead02d1bc4462208434fb2cbe7ecf45d67
朱国华 <zgh_email@163.com>
2020-01-17 17:20:05 +0800

    横向控制添加滑模控制

--- Control/LatControl/lat_control.cpp
+++ Control/LatControl/lat_control.cpp
@@ -23,12 +23,103 @@ void LatControl::Init()
 
 void LatControl::Proc(MessageManager *msg,VehicleController *ctl,PID *pid)
 {
-	float pid_val;
 	if(ctl->SteeringEnable)
 	{
-		pid->Desired = ctl->SteeringAngle;
-		pid_val = pid->pidUpdateIntegralSeparation(msg->SteeringAngle);
-		ctl->TurnTorqueVal = fabs(pid_val);
-		ctl->TurnTorqueDir = pid_val > 0 ? 0:1;
+		ctl->SteeringAngle = 0;
 	}
 }
+
+float LatControl::TargetLine(float x)
+{
+	return cosf(COEFFICIENT_TLS*x)-1;
+}
+
+float LatControl::TargetLineFirstDerivative(float x)
+{
+	return -COEFFICIENT_TLS*sinf(COEFFICIENT_TLS*x);
+}
+
+float LatControl::TargetLineSecondDerivative(float x)
+{
+	return -COEFFICIENT_TLS*COEFFICIENT_TLS*cosf(COEFFICIENT_TLS*x);
+}
+
+float LatControl::SatFunction(float x)
+{
+	if(x > COEFFICIENT_DELTA)
+	{
+		return  1.0f;
+	}
+	else if(x < -COEFFICIENT_DELTA)
+	{
+		return -1.0f;
+	}
+	else
+	{
+		return x/COEFFICIENT_DELTA;
+	}
+}
+
+void LatControl::Proc(MessageManager *msg,VehicleController *ctl,GeometricTrack *track)
+{
+	float psi_r,delta_r;
+	float cos_psi,cos_psi_r;
+	float tan_psi,tan_psi_r;
+
+	float delta_ctl;
+	float temp_a,temp_b;
+
+	if(ctl->SteeringEnable)
+	{
+		_target_track.setX(track->getPosition().getX());
+		_target_track.setY(TargetLine(_target_track.getX()));
+
+		_x1 = _target_track.getY() - track->getPosition().getY();
+		psi_r = atanf(TargetLineFirstDerivative(track->getPosition().getX()));
+
+		cos_psi_r = cosf(psi_r);
+		tan_psi_r = tanf(psi_r);
+
+		cos_psi   = cosf(track->getYaw());
+		tan_psi   = tanf(track->getYaw());
+
+		delta_r = atanf(WHEEL_BASE*cos_psi_r*cos_psi_r*cos_psi_r*TargetLineSecondDerivative(track->getPosition().getX()));
+
+		if(Drive == msg->getGear())
+		{
+			_x2 = tan_psi_r - tan_psi;
+		}
+		else if(Reverse == msg->getGear())
+		{
+			_x2 = tan_psi - tan_psi_r;
+		}
+		else
+		{
+
+		}
+		_sliding_variable = COEFFICIENT_SMV * _x1 + _x2;
+
+		temp_a = WHEEL_BASE * cos_psi * cos_psi * cos_psi;
+		temp_b = tanf(delta_r)/(WHEEL_BASE * cos_psi_r * cos_psi_r * cos_psi_r) +
+				COEFFICIENT_SMV * _x2 +
+				COEFFICIENT_RHO * SatFunction(_sliding_variable) +
+				COEFFICIENT_K * _sliding_variable;
+
+		delta_ctl = atanf(temp_a * temp_b);
+
+		ctl->SteeringAngle 		= delta_ctl*16*57.3f;
+		ctl->SteeringAngleRate 	= MAX_STEERING_ANGLE_RATE;
+	}
+}
+
+Vector2d LatControl::getTargetTrack()               { return  _target_track;}
+void     LatControl::setTargetTrack(Vector2d value) { _target_track = value;}
+
+float LatControl::getX1()            { return  _x1;}
+void  LatControl::setX1(float value) { _x1 = value;}
+
+float LatControl::getX2()            { return  _x2;}
+void  LatControl::setX2(float value) { _x2 = value;}
+
+float LatControl::getSlidingVariable()            { return  _sliding_variable;}
+void  LatControl::setSlidingVariable(float value) { _sliding_variable = value;}
--- Control/LatControl/lat_control.h
+++ Control/LatControl/lat_control.h
@@ -12,6 +12,16 @@
 #include "property.h"
 #include "controller.h"
 #include "pid.h"
+#include "math.h"
+#include "vector_2d.h"
+#include "GeometricTrack/geometric_track.h"
+#include "vehilce_config.h"
+
+#define COEFFICIENT_TLS 	( 0.4f  )	// 目标曲线曲率因子
+#define COEFFICIENT_SMV 	( 12.0f )	// 滑模变量系数
+#define COEFFICIENT_RHO 	( 0.02f )	// 补偿噪声系数
+#define COEFFICIENT_K   	( 1.1f  )	// 指数趋近率系数
+#define COEFFICIENT_DELTA	( 0.1f  ) // Sat函数系数
 
 class LatControl :public Controller
 {
@@ -21,7 +31,33 @@ public:
 
 	void Init() override;
 
+	float TargetLine(float x);
+	float TargetLineFirstDerivative(float x);
+	float TargetLineSecondDerivative(float x);
+
+	float SatFunction(float x);
+
 	void Proc(MessageManager *msg,VehicleController *ctl,PID *pid) override;
+	void Proc(MessageManager *msg,VehicleController *ctl,GeometricTrack *track);
+
+	Vector2d getTargetTrack();
+	void setTargetTrack(Vector2d value);
+
+	float getX1();
+	void  setX1(float value);
+
+	float getX2();
+	void  setX2(float value);
+
+	float getSlidingVariable();
+	void  setSlidingVariable(float value);
+
+private:
+	Vector2d _target_track;
+
+	float _x1;
+	float _x2;
+	float _sliding_variable;
 };
 
 #endif /* LATCONTROL_LAT_CONTROL_H_ */
